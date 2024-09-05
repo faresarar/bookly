764f9d4e91953de4756a086fe670396ff0a9f6fa
faresarar <faresarar455@hmail.com>
2024-09-05 21:48:12 +0100

    create splash_view_body.dart

--- lib/features/splash/presentation/views/splash_screen.dart
+++ lib/features/splash/presentation/views/splash_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'splash_view_body.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
   }
 }
